fce6e29b8d6578cf99e945cc5f77c4ab210b5f07
toraclinton16-dotcom <toraclinton16@gmail.com>
2026-07-25 03:15:06 +0300

    Update js/ranking.js with ranking display implementation

--- js/ranking.js
+++ js/ranking.js
@@ -3,6 +3,31 @@
  * Handles ranking and sorting of results
  */
 
+function updateRanking() {
+
+    const tbody = document.getElementById("rankingBody");
+
+    tbody.innerHTML = "";
+
+    const list = Object.values(marketData);
+
+    list.forEach((market, index) => {
+
+        const row = `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${market.symbol}</td>
+            <td>${market.price}</td>
+            <td>--</td>
+            <td>Scanning...</td>
+        </tr>`;
+
+        tbody.innerHTML += row;
+
+    });
+
+}
+
 const Ranking = {
     rankResults: function(results) {
         // Rank results by relevance or priority
