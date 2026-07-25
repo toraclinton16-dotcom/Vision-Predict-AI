3a763db41e77d1e16cfc85b4adabfd1896f5dd1a
toraclinton16-dotcom <toraclinton16@gmail.com>
2026-07-25 03:47:08 +0300

    Fix ranking display: show latest prices correctly and sort by price (highest first)

--- js/ranking.js
+++ js/ranking.js
@@ -11,15 +11,26 @@ function updateRanking() {
 
     const list = Object.values(marketData);
 
+    list.sort((a, b) => {
+        const priceA = a.prices[a.prices.length - 1] || 0;
+        const priceB = b.prices[b.prices.length - 1] || 0;
+        return priceB - priceA;
+    });
+
     list.forEach((market, index) => {
+
+        const latestPrice =
+            market.prices.length > 0
+                ? market.prices[market.prices.length - 1]
+                : "--";
 
         const row = `
         <tr>
             <td>${index + 1}</td>
             <td>${market.symbol}</td>
-            <td>${market.price}</td>
-            <td>--</td>
-            <td>Scanning...</td>
+            <td>${latestPrice}</td>
+            <td>Scanning</td>
+            <td>${SETTINGS.predictionDuration} Ticks</td>
         </tr>`;
 
         tbody.innerHTML += row;
